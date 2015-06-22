d5e51c1e66c3d6e5cf8b9846417dde1e535e11eb
TennyZhuang <zhuangtianyi0826@163.com>
2015-06-22 09:51:07 +0800

    add pairs generator

--- src/TestCaseGenerator.cpp
+++ src/TestCaseGenerator.cpp
@@ -5,7 +5,8 @@
 #include "TestCaseGenerator.h"
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <vector>
+#include <algorithm>
 #include <cstdlib>
 #include <ctime>
 
@@ -13,6 +14,9 @@
 namespace test {
 
     const int RECTANGLE_NUM_MAX = 500;
+    // WARNING: The implementation of random pair generator
+    // has some bug so RECTANGLE_NUM_MIN should not be less
+    // than PAIR_NUM;
     const int RECTANGLE_NUM_MIN = 300;
 
     const int RECTANGLE_WIDTH_MAX = 30;
@@ -20,6 +24,8 @@ namespace test {
 
     const int RECTANGLE_HEIGHT_MAX = 30;
     const int RECTANGLE_HEIGHT_MIN = 10;
+
+    const int PAIR_NUM = 250;
 
     TestCaseGenerator* TestCaseGenerator::generator_ = 0;
 
@@ -30,29 +36,43 @@ namespace test {
     }
 
     void TestCaseGenerator::generate(int cnt) {
-        std::srand((unsigned int)time(NULL));
+        std::srand((unsigned int)std::time(nullptr));
         std::ofstream fout;
 
         for (int i = 0; i < cnt; i++) {
+            // open file
             std::string file_name = "../test/data_in_" + std::to_string(i) + ".txt";
-
             fout.open(file_name, std::ios::out | std::ios::trunc | std::ios::binary);
 
             int n = RECTANGLE_NUM_MIN + std::rand() % (RECTANGLE_NUM_MAX - RECTANGLE_NUM_MIN);
 
             if (fout.is_open()) {
+                // write random rectangles
                 std::clog << "Writing data_in_" << i << ".txt" << std::endl;
                 for (int j = 0; j < n; j++) {
                     int width = RECTANGLE_WIDTH_MIN     + std::rand() % (RECTANGLE_WIDTH_MAX - RECTANGLE_WIDTH_MIN);
                     int height = RECTANGLE_HEIGHT_MIN + std::rand() % (RECTANGLE_HEIGHT_MAX - RECTANGLE_WIDTH_MIN);
                     fout << width << ' ' << height << std::endl;
                 }
+
+                // rectangle ending tag
+                fout << "0 0" << std::endl;
+
+                // make random pairs
+                // TODO: The random pairs generator may has a better implementation
+                std::vector <int> rectangle_ids;
+                for (int j = 1; j <= n; j++)
+                    rectangle_ids.push_back(j);
+                std::random_shuffle(rectangle_ids.begin(), rectangle_ids.end());
+                for (int j = 0; j < PAIR_NUM; j++)
+                    fout << rectangle_ids[j] << ' ' << rectangle_ids[j + 1] << std::endl;
             }
             else {
                 std::cerr << "ERROR: FILE NOT OPEN!" << std::endl;
                 exit(-1);
             }
 
+            // close
             fout.close();
         }
     }
