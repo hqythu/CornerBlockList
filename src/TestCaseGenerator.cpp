aa9073a3fc2adbc8d4aeebbe83a62f5c03aa54d1
TennyZhuang <zhuangtianyi0826@163.com>
2015-06-22 13:04:50 +0800

    bug fix

--- src/TestCaseGenerator.cpp
+++ src/TestCaseGenerator.cpp
@@ -12,6 +12,14 @@
 #include <cstdlib>
 #include <ctime>
 
+// a function for check pairs
+bool pred(const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
+    return !((p1.first != p1.second) &&
+             (p2.first != p2.second) &&
+             ((p1.first != p2.first) || (p1.second != p2.second)) &&
+             ((p1.first != p2.second) || (p1.second != p2.first)));
+}
+
 namespace test {
 
     TestCaseGenerator* TestCaseGenerator::generator_ = 0;
@@ -34,8 +42,10 @@ namespace test {
             int n = RECTANGLE_NUM_MIN + std::rand() % (RECTANGLE_NUM_MAX - RECTANGLE_NUM_MIN);
 
             if (fout.is_open()) {
-                // write random rectangles parameters
+                // write random rectangles' number
                 fout << n << std::endl;
+
+                // write random rectangles parameters
                 std::clog << "Writing data_in_" << i << ".txt" << std::endl;
                 for (int j = 0; j < n; j++) {
                     int width = RECTANGLE_WIDTH_MIN     + std::rand() % (RECTANGLE_WIDTH_MAX - RECTANGLE_WIDTH_MIN);
@@ -55,15 +65,13 @@ namespace test {
                 for (int j = 0; j < 2 * PAIR_NUM; j++)
                     pairs.push_back(std::make_pair(std::rand() % n + 1, std::rand() % n + 1));
 
-                // unuique
-                pairs.erase(std::unique(pairs.begin(), pairs.end()), pairs.end());
+                // unique
+                pairs.erase(std::unique(pairs.begin(), pairs.end(), pred), pairs.end());
 
                 // print
                 int count = 0;
-                for (auto pair : pairs) {
-                    if (count++ < PAIR_NUM && pair.first != pair.second)
-                        fout << pair.first << ' ' << pair.second << std::endl;
-                }
+                while (count++ < PAIR_NUM)
+                    fout << pairs[count].first << ' ' << pairs[count].second << std::endl;
             }
             else {
                 std::cerr << "ERROR: FILE NOT OPEN!" << std::endl;
